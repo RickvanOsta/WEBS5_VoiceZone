cd1d8b71d5f00efef1d4be8a51236acedf07ba34
raymondphua <chrphua@student.avans.nl>
2016-03-31 15:34:41 +0200

    Admin users detail

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
 
     // Populate the user table on initial page load
     populateTable();
-
+    // Username link click
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserDetail);
 });
 
 
@@ -22,6 +23,8 @@ function populateTable() {
 
     //jquery ajax call for JSON
     $.getJSON('/users', function(data) {
+
+        users = data;
 
         $.each(data, function() {
            tableContent += "<tr>";
@@ -35,4 +38,25 @@ function populateTable() {
         //inject content in table
         $('#userList table tbody').html(tableContent);
     });
+}
+
+
+function showUserDetail(event) {
+
+    //Prevent link from firing
+    event.preventDefault();
+
+    //retrieve username from link rel attribute
+    var selectedUsername = $(this).attr('rel');
+
+    //get index of object based on id value
+    var arrayPosition = users.map(function(arrayItem) { return arrayItem.username; }).indexOf(selectedUsername);
+
+    //get user from list
+    var user = users[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoUsername').text(user.username);
+    $('#userInfoFirstName').text(user.firstname);
+    $('#userInfoLastName').text(user.lastname);
 }
