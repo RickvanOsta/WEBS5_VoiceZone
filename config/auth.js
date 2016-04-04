c101c98cb7bcb4afa8c6f5bb9cf8f9ac330f3ce1
Rick van Osta <osta1995@hotmail.com>
2016-04-04 11:34:07 +0200

    fix

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '958223090931562', // your App ID
         'clientSecret'  : 'e6ca8129da6fb11d1ec44b6cdd62a439', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://voicezone.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
